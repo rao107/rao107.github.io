6723c566429e6f05d41f35349503943126d62ff5
Anirudh Rao <rao107@purdue.edu>
2021-06-16 18:22:41 -0700

    Website Revamp

---
+++ static/index.js
@@ -0,0 +1,12 @@
+let backgroundX = 0;
+let backgroundY = 0;
+
+bgPos = () => {
+    backgroundX += (Math.random() * 75 + 25) % 100;
+    backgroundY += (Math.random() * 75 + 25) % 100;
+
+    document.body.style.setProperty("background-position-x", String(backgroundX) + "%");
+    document.body.style.setProperty("background-position-y", String(backgroundY) + "%");
+}
+
+setInterval(bgPos, 1000);

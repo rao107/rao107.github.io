85b7616bb47579563c51a3cb2f0a225026756bc9
Anirudh Rao <rao107@purdue.edu>
2021-06-18 00:36:48 -0700

    Fixed bug so backgroundX and backgroundY are <100%

--- static/index.js
+++ static/index.js
@@ -2,8 +2,10 @@ let backgroundX = 0;
 let backgroundY = 0;
 
 bgPos = () => {
-    backgroundX += (Math.random() * 75 + 25) % 100;
-    backgroundY += (Math.random() * 75 + 25) % 100;
+    backgroundX += (Math.random() * 75 + 25);
+    backgroundX %= 100;
+    backgroundY += (Math.random() * 75 + 25);
+    backgroundY %= 100;
 
     document.body.style.setProperty("background-position-x", String(backgroundX) + "%");
     document.body.style.setProperty("background-position-y", String(backgroundY) + "%");

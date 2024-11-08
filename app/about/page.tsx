19cd41c512adcbf9841f78e994a7c7abc26e4348
Anirudh Rao <rao107@purdue.edu>
2024-11-07 22:37:36 -0800

    Create initial landing page + placeholder about and blog

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto temporibus placeat modi. Suscipit quas magnam quam dolorum assumenda inventore. Blanditiis assumenda quia consectetur iure tenetur laudantium repellendus nesciunt, velit autem.
+      </p>
+      <Link href='/'>Back</Link>
+    </div>
+  );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <Link href='/'>Back</Link>
+    </div>
+  );
+}
